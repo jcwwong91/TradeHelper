e40f71c881b3b4b965c2d3edadff2bc089e6c9d4
jcwwong91 <jcwwong91@gmail.com>
2016-10-16 15:54:25 -0700

    Added basic web calls

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -16,6 +18,76 @@ var(
 	web = flag.String("web", "/web", "The directory of static files for the web to serve" )
 )
 
+type handlerError struct {
+	err error
+	code int
+}
+
+type handler func(w http.ResponseWriter, r *http.Request) (interface{}, *handlerError)
+
+// attach the standard ServeHTTP method to our handler so the http library can call it
+func (fn handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// here we could do some prep work before calling the handler if we wanted to
+
+	// call the actual handler
+	response, err := fn(w, r)
+
+	// check for errors
+	if err != nil {
+		log.Printf("ERROR: %v\n", err.err)
+		http.Error(w, fmt.Sprintf(`{"error":"%s"}`, err.err), err.code)
+		return
+	}
+	if response == nil {
+		log.Printf("ERROR: response from method is nil\n")
+		http.Error(w, "Internal server error. Check the logs.", http.StatusInternalServerError)
+		return
+	}
+
+	// turn the response into JSON
+	bytes, e := json.Marshal(response)
+	if e != nil {
+		http.Error(w, "Error marshalling JSON", http.StatusInternalServerError)
+		return
+	}
+
+	// send the response and log
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(bytes)
+	log.Printf("%s %s %s %d", r.RemoteAddr, r.Method, r.URL, 200)
+}
+
+func getStock(w http.ResponseWriter, r *http.Request) (interface{}, *handlerError) {
+	ticker := mux.Vars(r)["ticker"]
+	log.Println("Getting", ticker)
+	return ticker, nil
+}
+
+func addStock(w http.ResponseWriter, r *http.Request) (interface{}, *handlerError) {
+	ticker := mux.Vars(r)["ticker"]
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, &handlerError{err, 500}
+	}
+
+	var payload struct {
+		// TODO: Extend with internal
+	}
+
+	if err = json.Unmarshal(data, &payload); err != nil {
+		return nil, &handlerError{err, 500}
+	}
+
+	log.Println("Tracking", ticker, "with", payload, "settings")
+	return payload, nil
+}
+
+func deleteStock(w http.ResponseWriter, r *http.Request) (interface{}, *handlerError) {
+	ticker := mux.Vars(r)["ticker"]
+	log.Println("Removing", ticker)
+	return ticker, nil
+}
+
 func main() {
 
 	flag.Parse()
@@ -27,6 +99,9 @@ func main() {
 	// setup routes
 	router := mux.NewRouter()
 	router.Handle("/", http.RedirectHandler("/static/", 302))
+	router.Handle("/stocks/{ticker}", handler(getStock)).Methods("GET")
+	router.Handle("/stocks/{ticker}", handler(addStock)).Methods("POST")
+	router.Handle("/stocks/{ticker}", handler(deleteStock)).Methods("DELETE")
 	router.PathPrefix("/static/").Handler(http.StripPrefix("/static", fileHandler))
 	http.Handle("/", router)
 
