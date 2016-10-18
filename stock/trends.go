c809a35cd121784fdceec61498a657f162d131b9
jcwwong91 <jcwwong91@gmail.com>
2016-10-17 19:58:39 -0700

    Added trend tracking

---
+++ stock/trends.go
@@ -0,0 +1,10 @@
+package stock
+
+// Trend represents a linear function about a particular trend a stock has
+type Trend struct {
+	Hits int		// Number of poitns a maxima/minima has hit the trend
+	Slope float64		// Slope of linear function
+	Constant float64	// Constant of linear function
+	Points []*Point		// Points that hit trend
+}
+
